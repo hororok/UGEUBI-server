4669369cd16baa64bd1631da5de3b6febaf305e0
kwonsye <kwonsye@naver.com>
2020-06-04 18:43:09 +0900

    User 도메인 설계 - User entity 생성 - 패스워드 수정 로직 추가 - 패스워드 암호화를 위한 BCrypt dependency 추가

--- src/main/java/duksung/backend/hororok/ugeubi/user/domain/entity/User.java
+++ src/main/java/duksung/backend/hororok/ugeubi/user/domain/entity/User.java
@@ -1,13 +1,42 @@
 package duksung.backend.hororok.ugeubi.user.domain.entity;
 
 import duksung.backend.hororok.ugeubi.common.domain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
-@Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+@Entity
 public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String username;
+
+    @Builder
+    private User(String email, String password, String phoneNumber, String username){
+        this.email=email;
+        this.password=password;
+        this.phoneNumber=phoneNumber;
+        this.username=username;
+    }
+
+    public void modifyPassword(String password){ this.password= BCrypt.hashpw(password,BCrypt.gensalt()); }
+    public boolean isNotEqualToPassword(String password){ return BCrypt.checkpw(password, this.password); }
+
 }
